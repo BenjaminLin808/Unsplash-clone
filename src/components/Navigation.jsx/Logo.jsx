eeedbf4e4ad7eeddd33ec0d63bb9ec17e6d6d1de
BenjaminLin808 <benjaminlin808@gmail.com>
2024-02-12 17:51:10 -0800

    add unsplash logo, create nav component outline

---
+++ src/components/Navigation.jsx/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import unsplashIcon from "../../assets/unsplashIcon.png";
+
+const Logo = () => {
+  return (
+    <a title="Home - Unsplash" href="/">
+      <img className="logo" src={unsplashIcon} alt="unsplash icon" />
+    </a>
+  );
+};
+
+export default Logo;
---
+++ src/components/Navigation.jsx/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "./Logo";
+
+import "./index.css";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <Logo />
+    </nav>
+  );
+};
+
+export default Navigation;
